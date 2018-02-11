2d961d954cb105ac9706f3c9d1376f122f7f326b
Andrew Reece <azmreece@gmail.com>
2018-02-11 15:35:15 +0000

    change live variable naming scheme

--- live_variable.h
+++ live_variable.h
@@ -2,38 +2,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#ifndef DEBUG_VARIABLE_TYPES
-#error You have to #define DEBUG_VARIABLE_TYPES! Do so in this format: `DEBUG_VARIABLE_TYPE(type, printf_format_string)`
+#ifndef DEBUG_LIVE_TYPES
+#error You have to #define DEBUG_LIVE_TYPES! Do so in this format: `DEBUG_LIVE_TYPE(type, printf_format_string)`
 #endif
-#ifndef DEBUG_VARIABLE_LISTINGS
-#error You have to #define DEBUG_VARIABLE_LISTINGS! Do so in this format: `DEBUG_VARIABLE_LISTING(type, name)`
+#ifndef DEBUG_LIVE_VARS
+#error You have to #define DEBUG_LIVE_VARS! Do so in this format: `DEBUG_LIVE_VAR(type, name)`
 #endif
 
-#define DEBUG_VARIABLE_STRUCT(type, format, ...) DEBUG_VARIABLE_TYPE(type, format)
+#define DEBUG_LIVE_STRUCT(type, format, ...) DEBUG_LIVE_TYPE(type, format)
 typedef struct debug_variable
 {
 	enum {
-#define DEBUG_VARIABLE_TYPE(type, format) \
+#define DEBUG_LIVE_TYPE(type, format) \
 		DebugVarType_## type,
-		DEBUG_VARIABLE_TYPES
-#undef DEBUG_VARIABLE_TYPE
+		DEBUG_LIVE_TYPES
+#undef DEBUG_LIVE_TYPE
 	} Type;
 	char *Name;
 	union {
-#define DEBUG_VARIABLE_TYPE(type, format) \
+#define DEBUG_LIVE_TYPE(type, format) \
 		type Value_## type;
-		DEBUG_VARIABLE_TYPES
-#undef DEBUG_VARIABLE_TYPE
+		DEBUG_LIVE_TYPES
+#undef DEBUG_LIVE_TYPE
 	};
 } debug_variable;
-#undef DEBUG_VARIABLE_STRUCT
+#undef DEBUG_LIVE_STRUCT
 
 debug_variable DebugVariables[] = {
-#define DEBUG_VARIABLE_LISTING(type, Name) \
+#define DEBUG_LIVE_VAR(type, Name) \
 	{ DebugVarType_## type, #Name, 0 },
 	// NOTE: this only sets the first type listed (suggested integral type for toggling)
-	DEBUG_VARIABLE_LISTINGS
-#undef DEBUG_VARIABLE_LISTING
+	DEBUG_LIVE_VARS
+#undef DEBUG_LIVE_VAR
 };
 
 typedef struct debug_var_state
@@ -54,11 +54,11 @@ static void
 UpdateCompiledVariables()
 {
 	unsigned int iVar = 0;
-#define DEBUG_VARIABLE_LISTING(type, Name) \
+#define DEBUG_LIVE_VAR(type, Name) \
 	type TempDebugVar_## Name = DEBUGVAR_## Name; \
 	DebugVariables[iVar++].Value_## type = TempDebugVar_## Name;
-	DEBUG_VARIABLE_LISTINGS
-#undef  DEBUG_VARIABLE_LISTING
+	DEBUG_LIVE_VARS
+#undef  DEBUG_LIVE_VAR
 }
 
 static inline int
@@ -89,15 +89,13 @@ RecompileOnRequest(char *BuildCall, char *Filename)
 			debug_variable Var = DebugVariables[iVar];
 			switch(Var.Type)
 			{
-#define DEBUG_STRUCT(type) Var.Value_## type.
-#define DEBUG_VAL(type) Var.Value_## type
-#define DEBUG_VARIABLE_TYPE(type, format) DEBUG_VARIABLE_STRUCT(type, format, Var.Value_## type)
-#define DEBUG_VARIABLE_STRUCT(type, format, ...) case DebugVarType_## type: fprintf(File, "#define DEBUGVAR_%s "format"\n", Var.Name, __VA_ARGS__); break;
-				DEBUG_VARIABLE_TYPES
-#undef DEBUG_VARIABLE_TYPE
-#undef DEBUG_VARIABLE_STRUCT
-#undef DEBUG_STRUCT
-#undef DEBUG_VAL
+#define DEBUG_LIVE_TYPE(type, format) DEBUG_LIVE_STRUCT(type, format, Var.Value_## type)
+#define DEBUG_LIVE_MEMBER(struct, member) Var.Value_## struct.member
+#define DEBUG_LIVE_STRUCT(type, format, ...) case DebugVarType_## type: fprintf(File, "#define DEBUGVAR_%s "format"\n", Var.Name, __VA_ARGS__); break;
+				DEBUG_LIVE_TYPES
+#undef DEBUG_LIVE_STRUCT
+#undef DEBUG_LIVE_MEMBER
+#undef DEBUG_LIVE_TYPE
 			}
 		}
 		fprintf(File, "#define LIVE_VAR_CONFIG_H\n#endif");

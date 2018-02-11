23634f2eeadae1f639a1e30294e7a6c566285e4d
Andrew Reece <azmreece@gmail.com>
2018-02-11 15:09:53 +0000

    add live structs

--- live_variable.h
+++ live_variable.h
@@ -9,6 +9,7 @@
 #error You have to #define DEBUG_VARIABLE_LISTINGS! Do so in this format: `DEBUG_VARIABLE_LISTING(type, name)`
 #endif
 
+#define DEBUG_VARIABLE_STRUCT(type, format, ...) DEBUG_VARIABLE_TYPE(type, format)
 typedef struct debug_variable
 {
 	enum {
@@ -25,10 +26,11 @@ typedef struct debug_variable
 #undef DEBUG_VARIABLE_TYPE
 	};
 } debug_variable;
+#undef DEBUG_VARIABLE_STRUCT
 
 debug_variable DebugVariables[] = {
 #define DEBUG_VARIABLE_LISTING(type, Name) \
-	{ DebugVarType_## type, #Name, (unsigned int) DEBUGVAR_## Name },
+	{ DebugVarType_## type, #Name, 0 },
 	// NOTE: this only sets the first type listed (suggested integral type for toggling)
 	DEBUG_VARIABLE_LISTINGS
 #undef DEBUG_VARIABLE_LISTING
@@ -52,7 +54,9 @@ static void
 UpdateCompiledVariables()
 {
 	unsigned int iVar = 0;
-#define DEBUG_VARIABLE_LISTING(type, Name) DebugVariables[iVar++].Value_## type = DEBUGVAR_## Name;
+#define DEBUG_VARIABLE_LISTING(type, Name) \
+	type TempDebugVar_## Name = DEBUGVAR_## Name; \
+	DebugVariables[iVar++].Value_## type = TempDebugVar_## Name;
 	DEBUG_VARIABLE_LISTINGS
 #undef  DEBUG_VARIABLE_LISTING
 }
@@ -85,9 +89,15 @@ RecompileOnRequest(char *BuildCall, char *Filename)
 			debug_variable Var = DebugVariables[iVar];
 			switch(Var.Type)
 			{
-#define DEBUG_VARIABLE_TYPE(type, format) case DebugVarType_## type: fprintf(File, "#define DEBUGVAR_%s "format"\n", Var.Name, Var.Value_## type); break;
+#define DEBUG_STRUCT(type) Var.Value_## type.
+#define DEBUG_VAL(type) Var.Value_## type
+#define DEBUG_VARIABLE_TYPE(type, format) DEBUG_VARIABLE_STRUCT(type, format, Var.Value_## type)
+#define DEBUG_VARIABLE_STRUCT(type, format, ...) case DebugVarType_## type: fprintf(File, "#define DEBUGVAR_%s "format"\n", Var.Name, __VA_ARGS__); break;
 				DEBUG_VARIABLE_TYPES
 #undef DEBUG_VARIABLE_TYPE
+#undef DEBUG_VARIABLE_STRUCT
+#undef DEBUG_STRUCT
+#undef DEBUG_VAL
 			}
 		}
 		fprintf(File, "#define LIVE_VAR_CONFIG_H\n#endif");

94adcb7665a1406d04d7d10da670bd32e5fc1895
Andrew Reece <azmreece@gmail.com>
2019-05-11 13:02:29 +0100

    Remove dynamic allocations

--- win32_live_edit.h
+++ win32_live_edit.h
@@ -1,14 +1,26 @@
+/* TODO:
+ *  - non-function symbols
+ *  - stop copying dlls - user can deal with making a temp
+ */
+
 #ifndef WIN32_LIVE_EDIT_H
+#ifndef Assert
+#include <assert.h>
+#define Assert assert
+#endif
 
 typedef void void_func();
 typedef int b32;
 typedef unsigned int uint;
 
-typedef struct library_function
+typedef struct library_symbol
 {
 	char *Name;
-	void_func *Function;
-} library_function;
+	union {
+		void_func *Function;
+		void *Data;
+	} Symbol;
+} library_symbol;
 
 #define WIN32_STATE_FILE_NAME_COUNT MAX_PATH
 typedef struct exe_info
@@ -31,7 +43,7 @@ typedef struct win32_library
 
 	// IMPORTANT: callbacks can be 0! Check before calling.
 	uint NumFunctions;
-	library_function *Functions;
+	library_symbol *Functions;
 
 	lib_filepaths Paths;
 	exe_info EXE;
@@ -39,14 +51,13 @@ typedef struct win32_library
 	b32 IsValid;
 } win32_library;
 
-#ifdef LIVE_EDIT_STANDALONE
 static void
 CatStrings(size_t SourceACount, char *SourceA,
 		   size_t SourceBCount, char *SourceB,
 		   size_t DestCount, char *Dest)
 {
 	// TODO: Dest bounds checking!
-	DestCount;
+	assert(DestCount >= SourceACount + SourceBCount + 1);
 	for(int Index = 0;
 			Index < SourceACount;
 			++Index)
@@ -104,7 +115,7 @@ Win32GetEXEFilename(char *PathNameBuf, int StringLen, char **Filename)
 	// NOTE: Never use MAX_PATH in code that is user-facing, because it
 	// can be dangerous and lead to bad results.
 	//DWORD SizeOfFilename =
-		GetModuleFileNameA(0, PathNameBuf, StringLen);
+		GetModuleFileName(0, PathNameBuf, StringLen);
 	*Filename = PathNameBuf;
 	for(char *Scan = PathNameBuf;
 			*Scan;
@@ -118,57 +129,68 @@ Win32GetEXEFilename(char *PathNameBuf, int StringLen, char **Filename)
 }
 
 static inline FILETIME
-Win32GetLastWriteTime(char *Filename)
+le__Get_last_write_time(char *Filename)
 {
 	FILETIME LastWriteTime = {0};
 
 	WIN32_FILE_ATTRIBUTE_DATA Data;
 	if(GetFileAttributesEx(Filename, GetFileExInfoStandard, &Data))
-	{
-		LastWriteTime = Data.ftLastWriteTime;
-	}
+	{ LastWriteTime = Data.ftLastWriteTime; }
 
 	return LastWriteTime;
 }
-#endif // LIVE_EDIT_STANDALONE
+
+static inline b32
+le__file_is_present(char *file_path)
+{
+	WIN32_FILE_ATTRIBUTE_DATA Ignored;
+	return !!GetFileAttributesEx(file_path, GetFileExInfoStandard, &Ignored);
+}
 
 /// 1 for success, 0 for failure
 static b32
 Win32LoadLib(win32_library *Lib)
 {
-	WIN32_FILE_ATTRIBUTE_DATA Ignored;
-	if(!GetFileAttributesEx(Lib->Paths.Lock, GetFileExInfoStandard, &Ignored))
+	if(! le__file_is_present(Lib->Paths.Lock))
 	{
-		Lib->DLLLastWriteTime = Win32GetLastWriteTime(Lib->Paths.SourceDLL);
+		Lib->DLLLastWriteTime = le__Get_last_write_time(Lib->Paths.SourceDLL);
 
-		CopyFileA(Lib->Paths.SourceDLL, Lib->Paths.TempDLL, FALSE);
+		int CopySuccess = CopyFile(Lib->Paths.SourceDLL, Lib->Paths.TempDLL, FALSE);
 
-		Lib->CodeDLL = LoadLibraryA(Lib->Paths.TempDLL);
-		if(Lib->CodeDLL)
+		Lib->CodeDLL = LoadLibrary(Lib->Paths.TempDLL);
+		// NOTE: if this fails, you may be trying to load an invalid path
+		if(Lib->CodeDLL && CopySuccess)
 		{
 			Lib->IsValid = 1;
-			for(uint FuncI = 0; FuncI < Lib->NumFunctions; ++FuncI)
+			for(uint sym_i = 0; sym_i < Lib->NumFunctions; ++sym_i)
 			{
-				Lib->Functions[FuncI].Function = (void_func *)
-					GetProcAddress(Lib->CodeDLL, Lib->Functions[FuncI].Name);
+				Lib->Functions[sym_i].Function = (void_func *)
+					GetProcAddress(Lib->CodeDLL, Lib->Functions[sym_i].Name);
 
-			if(!Lib->Functions[FuncI].Function) Lib->IsValid = 0;
+				// NOTE: if this triggers, you may not be exporting the function
+				// from the dll
+				if(! Lib->Functions[sym_i].Function) { Lib->IsValid = 0; }
 			}
+		}
+		else {
+			int ErrCode = GetLastError();
+			ErrCode += 0; // just for debugging, so that it is still in scope after being set
 		}
 	}
 
-	if(!Lib->IsValid)
+	if(! Lib->IsValid)
 	{
 		// TODO: invalidate each function individually? does that even make sense?
-		/* for(uint FuncI = 0; FuncI < Lib->NumFunctions; ++FuncI) */
+		/* for(uint sym_i = 0; sym_i < Lib->NumFunctions; ++sym_i) */
 		/* { */
-		/* 	Lib->Functions[FuncI].Function = 0; */
+		/* 	Lib->Functions[sym_i].Function = 0; */
 		/* } */
 		return 0;
 	}
 
 	return 1;
 }
+
 static void
 Win32UnloadLib(win32_library *Lib)
 {
@@ -179,9 +201,9 @@ Win32UnloadLib(win32_library *Lib)
 	}
 
 	Lib->IsValid = 0;
-	for(uint FuncI = 0; FuncI < Lib->NumFunctions; ++FuncI)
+	for(uint sym_i = 0; sym_i < Lib->NumFunctions; ++sym_i)
 	{
-		Lib->Functions[FuncI].Function = 0;
+		Lib->Functions[sym_i].Function = 0;
 	}
 }
 
@@ -193,18 +215,18 @@ Win32GetLibraryPaths(win32_library *Lib, char *SourceDLL, char *TempDLL, char *L
 	Win32BuildPathFilename(Lib->EXE.PathName, Lib->EXE.Filename_FromPath, TempDLL,
 			     		  sizeof(Lib->Paths.TempDLL), Lib->Paths.TempDLL);
 	Win32BuildPathFilename(Lib->EXE.PathName, Lib->EXE.Filename_FromPath, Lock,
-							  sizeof(Lib->Paths.Lock), Lib->Paths.Lock);
+						  sizeof(Lib->Paths.Lock), Lib->Paths.Lock);
 }
 
 static b32
 Win32ReloadLibOnRecompile(win32_library *Lib)
 {
 	b32 LibLoaded = 0;
-	FILETIME NewDLLWriteTime = Win32GetLastWriteTime(Lib->Paths.SourceDLL);
-	WIN32_FILE_ATTRIBUTE_DATA IgnoredFileData;
+	FILETIME new_dll_write_time = le__Get_last_write_time(Lib->Paths.SourceDLL);
+	b32 dll_has_been_changed    = CompareFileTime(&new_dll_write_time, &Lib->DLLLastWriteTime) != 0;
+	b32 lock_has_been_removed   = ! le__file_is_present(Lib->Paths.Lock);
 	// See if file has been changed and the lockfile has been removed
-	if(CompareFileTime(&NewDLLWriteTime, &Lib->DLLLastWriteTime) != 0 &&
-		!GetFileAttributesEx(Lib->Paths.Lock, GetFileExInfoStandard, &IgnoredFileData))
+	if(dll_has_been_changed && lock_has_been_removed)
 	{
 		Win32UnloadLib(Lib);
 		LibLoaded = Win32LoadLib(Lib);
@@ -214,22 +236,14 @@ Win32ReloadLibOnRecompile(win32_library *Lib)
 }
 
 static win32_library
-Win32LibraryFunctions(char **FnNames, int NumFns)
-{
-	win32_library Lib = {0};
-	Lib.Functions = VirtualAlloc(0, NumFns * sizeof(*Lib.Functions), MEM_RESERVE|MEM_COMMIT, PAGE_READWRITE);
-	/* Lib.Functions = calloc(NumFns * sizeof(*Lib.Functions)); */
-	for(int FuncI = 0; FuncI < NumFns; ++FuncI)
-	{ Lib.Functions[FuncI].Name = FnNames[FuncI]; }
-	return Lib;
-}
-
-static win32_library
-Win32Library(char **FnNames, int NumFns,
+Win32Library(library_symbol *fns, uint fns_n,
 			b32 CompletePaths, char *SourcePath, char *TempPath, char *LockPath)
 {
-	win32_library Lib = Win32LibraryFunctions(FnNames, NumFns);
-	Lib.NumFunctions = NumFns;
+	win32_library Lib = {0};
+	{
+		Lib.NumFunctions = syms_n;
+		Lib.Functions = syms;
+	}
 	Win32GetEXEFilename(Lib.EXE.PathName, sizeof(Lib.EXE.PathName), &Lib.EXE.Filename_FromPath);
 
 	if(CompletePaths)
